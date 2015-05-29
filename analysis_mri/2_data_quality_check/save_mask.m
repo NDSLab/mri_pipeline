b9fb1fbba8f01e0b42e052acea0baf7ff19dce90
Peter Vavra <peter.vavra.1984@gmail.com>
2015-05-29 17:49:02 +0200

    Clean up of data quality scripts - Movies and signal intensity work; spike detection/removal not implemented yet

--- analysis_mri/2_data_quality_check/save_mask.m
+++
@@ -1,6 +0,0 @@
-function save_mask(mask,imgs_headers,cfg,iRun)
-header = imgs_headers{1};
-[pth,nm,xt] = fileparts(deblank(header.fname));
-header.fname = fullfile(cfg.spike_dir,[sprintf('CheckSpike_Mask_run%i',iRun) xt]);
-header.descrip = ('CheckSpike_Mask');
-spm_write_vol(header, mask);
