b92c8fa05eed13d8e68fcc75864d3165b68e3de3
Peter Vavra <peter.vavra.1984@gmail.com>
2015-06-03 15:05:50 +0200

    enforce CheckSubject script run on Windows to use H264 codec; also, run for each run separately

--- analysis_mri/2_data_quality_check/batch_CheckSubject.m
+++ analysis_mri/2_data_quality_check/batch_CheckSubject.m
@@ -1,27 +1,15 @@
 function batch_CheckSubject()
 %BATCH_CHECKSUBJECT runs CheckSubject.m for an array of subjects.
-% Unfortunately, torque doesn't want to run the CheckSubject.m script.
-% I suspsect because it doesn't handle 
+% Unfortunately, CheckSubject cannot be run on a linux machine (because of
+% the videos) so you have to run this script on a windows PC.
 %
-% Usage:    open an interactive Matlab session and just hit "F5" after
-%           editing the "subjects" array.
+% Usage:    after editing the "subjects" array.open an interactive Matlab
+%           session and just hit "F5"
 
-subjects = [2 3 7:20]; % array of integers
+subjects = 1:2; % array of integers
 
-% 
-% % human readable requirements for single job:
-% memory_in_GB = 2;
-% time_in_hours  = 2;
-% 
-% cfg.memreq = memory_in_GB * 1024 *1024 * 1024;
-% cfg.timreq = time_in_hours * 60 * 60;
-
-addpath /home/common/matlab/fieldtrip/qsub
-if ~isempty(subjects)
-    for s = subjects
-        % submit one job at a time
-        feval(@CheckSubject, s);
-    end
-end
+for s = subjects
+    feval(@CheckSubject, s);
+    
 end
 
