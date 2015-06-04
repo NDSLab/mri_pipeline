7c70e1dd5f107bc7c198174c3c6585755751f9bc
Inge Huijsmans <i.huijsmans@donders.ru.nl>
2015-06-04 14:14:33 +0200

    revert changes :q! Revert "Changes in spike detection Inge Huijsmans 1/6/2015"
    This reverts commit 2e13ddffee593a27b329b5dccce7a557c72da352.

--- analysis_mri/2_data_quality_check/save_mask.m
+++ analysis_mri/2_data_quality_check/save_mask.m
@@ -1,21 +1,6 @@
 function save_mask(mask,imgs_headers,cfg,iRun)
-%    
-% This funtion saves the mask as spm volume made by getmask.m
-% Mask is saved in spike directory
-%
-% Input
-% mask: from getmask.m
-% imgs_headers: volume information from spm_vol
-% cfg: information (INFO)
-% iRun: spikes from which run.
-    
-    %Create volume information from original volumes
-    header = imgs_headers{1};
-    [~,~,ext] = fileparts(deblank(header.fname));
-    header.fname = fullfile(cfg.spike_dir,[sprintf('CheckSpike_Mask_run%i',iRun) ext]);
-    header.descrip = ('CheckSpike_Mask');
-    
-    %Save mask volume information
-    spm_write_vol(header, mask);
-    
-end
+header = imgs_headers{1};
+[pth,nm,xt] = fileparts(deblank(header.fname));
+header.fname = fullfile(cfg.spike_dir,[sprintf('CheckSpike_Mask_run%i',iRun) xt]);
+header.descrip = ('CheckSpike_Mask');
+spm_write_vol(header, mask);
--- analysis_mri/2_data_quality_check/save_spikefile.m
+++ analysis_mri/2_data_quality_check/save_spikefile.m
@@ -1,14 +1,7 @@
 function save_spikefile(affected_vol_slc, affected_vol, imgs_headers,cfg,iRun)
-%
-% Creates filenames and saves affected volumes & slices.
-%
-    
-    %Create new filename
-    [~,~,xt] = fileparts(deblank(imgs_headers{1}.fname));
-    fullname_vol = fullfile(cfg.spike_dir,[sprintf('CheckSpike_AffectedVols_run%i',iRun),xt]);
-    fullname_vol_slc = fullfile(cfg.spike_dir,[sprintf('CheckSpike_AffectedVolsSlices_run%i',iRun) xt]);
 
-    %Save affected volumes & slices
-    save(fullname_vol,'affected_vol');
-    save(fullname_vol_slc,'affected_vol_slc');
-end
+[pth,nm,xt] = fileparts(deblank(imgs_headers{1}.fname));
+fullname = fullfile(cfg.spike_dir,[sprintf('CheckSpike_AffectedVols_run%i',iRun),xt]);
+save(fullname,'affected_vol');
+fullname = fullfile(cfg.spike_dir,[sprintf('CheckSpike_AffectedVolsSlices_run%i',iRun) xt]);
+save(fullname,'affected_vol_slc');
