0b56fd77856480418b3dd789ad53d402ebdc2dc6
Inge Huijsmans <i.huijsmans@donders.ru.nl>
2015-06-11 15:51:28 +0200

    rename file & update coding convention

--- analysis_mri/2_data_quality_check/batch_CheckSubject.m
+++ analysis_mri/2_data_quality_check/batch_CheckSubject.m
@@ -1,15 +1,16 @@
 function batch_CheckSubject()
-%BATCH_CHECKSUBJECT runs CheckSubject.m for an array of subjects.
-% Unfortunately, CheckSubject cannot be run on a linux machine (because of
-% the videos) so you have to run this script on a windows PC.
 %
-% Usage:    after editing the "subjects" array.open an interactive Matlab
-%           session and just hit "F5"
+% Runs CheckSubject.m for an array of subjects.
+% Adjust array of subjects
 
-subjects = 1:2; % array of integers
+    subjects = [101]; % array of integers
 
-for s = subjects
-    feval(@CheckSubject, s);
-    
+    addpath /home/common/matlab/fieldtrip/qsub
+    if ~isempty(subjects)
+        for s = subjects
+            % submit one job at a time
+            feval(@CheckSubject, s);
+        end
+    end
 end
 
